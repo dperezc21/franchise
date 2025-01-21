ba9d3f121e4e6771c882ee7914fb027435bb5281
davier enrique perez cabrera <davierperez11.2@gmail.com>
2025-01-21 00:18:07 -0500

    feat: controller to welcome

---
+++ src/main/java/com/prueba/franquicia/infrastructure/controllers/Controller.java
@@ -0,0 +1,17 @@
+package com.prueba.franquicia.infrastructure.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class Controller {
+
+    @GetMapping
+    public ResponseEntity<?> welcome() {
+        return new ResponseEntity<String>(HttpStatus.OK);
+    }
+}

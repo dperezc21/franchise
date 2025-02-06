5490d8287754bdf2d6735357d008a862e5dd4f31
davier enrique perez cabrera <davierperez11.2@gmail.com>
2025-02-05 23:02:50 -0500

    test: test branch file generated

---
+++ src/test/java/com/prueba/franquicia/application/BranchUseCaseTest.java
@@ -0,0 +1,17 @@
+package com.prueba.franquicia.application;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BranchUseCaseTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getBranchById() {
+    }
+}
